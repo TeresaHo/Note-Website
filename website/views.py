72dd0a1b08e16de2e1e1e54fa86aa147c6ce10ef
Teresa Ho <teresa1995885@gmail.com>
2022-06-22 23:10:56 +0200

    add other templates

--- website/views.py
+++ website/views.py
@@ -1,8 +1,8 @@
-from flask import Blueprint
+from flask import Blueprint, render_template
 
 views = Blueprint('views',__name__)
 
 @views.route('/') #at the name of your Blueprint
 def home():
-    return "<h1> Test </h1>"
+    return render_template("home.html") # Just need to put in the name of the template no need to include path to the file
 # need to register blueprints in __init__.py
